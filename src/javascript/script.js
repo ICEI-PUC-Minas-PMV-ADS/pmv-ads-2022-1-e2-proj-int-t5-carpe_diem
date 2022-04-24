72b5d442a8aa8aca020b296c122a9d07bb126ee0
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2022-04-24 15:19:23 -0300

    homepage

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,179 +1,42 @@
-$('.slider').each(function() {
-
-    var $this = $(this);
-
-    var $group = $this.find('.slide_group');
-
-    var $slides = $this.find('.slide');
-
-    var bulletArray = [];
-
-    var currentIndex = 0;
-
-    var timeout;
-
-    
-
-    function move(newIndex) {
-
-      var animateLeft, slideLeft;
-
-      
-
-      advance();
-
-      
-
-      if ($group.is(':animated') || currentIndex === newIndex) {
-
-        return;
-
-      }
-
-      
-
-      bulletArray[currentIndex].removeClass('active');
-
-      bulletArray[newIndex].addClass('active');
-
-      
-
-      if (newIndex > currentIndex) {
-
-        slideLeft = '100%';
-
-        animateLeft = '-100%';
-
-      } else {
-
-        slideLeft = '-100%';
-
-        animateLeft = '100%';
-
-      }
-
-      
-
-      $slides.eq(newIndex).css({
-
-        display: 'block',
-
-        left: slideLeft
-
-      });
-
-      $group.animate({
-
-        left: animateLeft
-
-      }, function() {
-
-        $slides.eq(currentIndex).css({
-
-          display: 'none'
-
-        });
-
-        $slides.eq(newIndex).css({
-
-          left: 0
-
-        });
-
-        $group.css({
-
-          left: 0
-
-        });
-
-        currentIndex = newIndex;
-
-      });
-
-    }
-
-    
-
-    function advance() {
-
-      clearTimeout(timeout);
-
-      timeout = setTimeout(function() {
-
-        if (currentIndex < ($slides.length - 1)) {
-
-          move(currentIndex + 1);
-
-        } else {
-
-          move(0);
-
-        }
-
-      }, 4000);
-
-    }
-
-    
-
-    $('.next_btn').on('click', function() {
-
-      if (currentIndex < ($slides.length - 1)) {
-
-        move(currentIndex + 1);
-
-      } else {
-
-        move(0);
-
-      }
-
-    });
-
-    
-
-    $('.previous_btn').on('click', function() {
-
-      if (currentIndex !== 0) {
-
-        move(currentIndex - 1);
-
-      } else {
-
-        move(3);
-
-      }
-
-    });
-
-    
-
-    $.each($slides, function(index) {
-
-      var $button = $('<a class="slide_btn">&bull;</a>');
-
-      
-
-      if (index === currentIndex) {
-
-        $button.addClass('active');
-
-      }
-
-      $button.on('click', function() {
-
-        move(index);
-
-      }).appendTo('.slide_buttons');
-
-      bulletArray.push($button);
-
-    });
-
-    
-
-    advance();
-
-  });
-script.js
-A mostrar script.js.
+cards_places.innerHTML = ''
+
+function card(img, lugar, regiao, qt_quarto, qt_hospede, qt_banheiro, price){
+  card_html = `
+  <div class="flex gap-3 w-f hover:shadow-3 transition" id="card_places">
+      <div class="flex-1 shadow-1"> 
+          <img class="w-f h-2 object-cover" src="${img}"> 
+          <p class="p-1 bold-7 t-center font-size-5">${regiao}</p>     
+          <div class="grid grid-col-2 p-1">
+              <div>
+                  <p class="flex"><span class="material-symbols-outlined pr-1">local_see</span> ${lugar}</p>
+                  <p class="flex"><span class="material-symbols-outlined pr-1">bed</span> ${qt_quarto} Quarto</p>
+              </div>
+              <div>
+                  <p class="flex"><span class="material-symbols-outlined pr-1">person</span> ${qt_hospede} hóspedes</p>
+                  <p class="flex"><span class="material-symbols-outlined pr-1">bathroom</span> ${qt_banheiro} Banheiro</p>
+              </div>
+          </div> 
+          <p class="bold-6 t-center p-1">A partir de R$ ${price} / noite</p>
+      </div>
+  </div>
+  `
+  div = document.createElement('div')
+  div.innerHTML = card_html
+
+  cards_places.appendChild(div.children[0])
+}
+
+card('img/hotel-1.jpg', 'Praia', "Porto Seguro - BA",  1, 5, 1, 180)
+card('img/hotel-2.jpg', 'Praia', "Vitória - ES",       1, 5, 1, 280)
+card('img/hotel-3.jpg', 'Praia', "São Luiz - MA",      1, 5, 1, 130)
+card('img/hotel-4.jpg', 'Praia', "Florianópolis - SC", 1, 5, 1, 980)
+
+
+// slideshow
+setInterval(()=>{ capa.scrollTo(0,0*400) },2*1000)
+setInterval(()=>{ capa.scrollTo(0,1*400) },4*1000)
+setInterval(()=>{ capa.scrollTo(0,2*400) },6*1000)
+setInterval(()=>{ capa.scrollTo(0,3*400) },8*1000)
+
+d = new Date()
+ano.innerHTML = d.getFullYear()
