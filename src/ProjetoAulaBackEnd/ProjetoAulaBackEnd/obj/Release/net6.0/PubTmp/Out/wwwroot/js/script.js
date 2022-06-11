3fb5edb756ba169960fe1e52f6b1c6bc1a21372e
Edu-DevBR <440744@sga.pucminas.br>
2022-06-11 15:38:41 -0300

    restauração

--- src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/obj/Release/net6.0/PubTmp/Out/wwwroot/js/script.js
+++ src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/obj/Release/net6.0/PubTmp/Out/wwwroot/js/script.js
@@ -1,42 +1,37 @@
-cards_places.innerHTML = ''
+// cards_places.innerHTML = ''
 
 function card(img, lugar, regiao, qt_quarto, qt_hospede, qt_banheiro, price, link){
-  card_html = `
-  <div class="flex gap-3 w-f hover:shadow-3 transition" id="card_places">
-      <div class="flex-1 shadow-1"> 
-          <a href='${link}'><img class="w-f h-2 object-cover" src="${img}"> </a>
-          <p class="p-1 bold-7 t-black t-center font-size-5">${regiao}</p>     
-          <div class="grid grid-col-2 p-1">
-              <div>
-                  <p class="flex"><span class="material-symbols-outlined pr-1">local_see</span> ${lugar}</p>
-                  <p class="flex"><span class="material-symbols-outlined pr-1">bed</span> ${qt_quarto} Quarto</p>
-              </div>
-              <div>
-                  <p class="flex"><span class="material-symbols-outlined pr-1">person</span> ${qt_hospede} hóspedes</p>
-                  <p class="flex"><span class="material-symbols-outlined pr-1">bathroom</span> ${qt_banheiro} Banheiro</p>
-              </div>
-          </div> 
-          <p class="bold-6 t-black t-center p-1">A partir de R$ ${price} / noite</p>     
-      </div>
-  </div>
-  `
+
+    let card_html = `<div class="card shadow-1 hover:shadow-3 transition" id="div_card">
+        <img class="card-top" src="${img}">
+        <div class="card-body">
+            <div class="card-t card-center card-title">${lugar}</div>
+            <div class="card-m">
+                <div class="card-center-y"> <span class="material-symbols-outlined pr-1">local_see</span>${regiao} </div>
+                <div class="card-center-y"> <span class="material-symbols-outlined pr-1">person</span>${qt_hospede} hóspedes </div>
+                <div class="card-center-y"> <span class="material-symbols-outlined pr-1">bed</span>${qt_quarto} Quarto </div>
+                <div class="card-center-y"> <span class="material-symbols-outlined pr-1">bathroom</span>${qt_banheiro} Banheiro </div>
+            </div>
+            <div class="card-b card-center"><b>A partir de R$ ${price} / noite</b></div>
+        </div>
+    </div>`
 
   div = document.createElement('div')
   div.innerHTML = card_html
 
   cards_places.appendChild(div.children[0])
+  
 }
 
-card('img/hotel-1.jpg', 'Praia', "Porto Seguro - BA"  , 1, 5, 1, 180, 'http://www.youtube.com')
-card('img/hotel-2.jpg', 'Praia', "Vitória - ES"       , 1, 5, 1, 280, 'http://www.google.com')
-card('img/hotel-3.jpg', 'Praia', "São Luiz - MA"      , 1, 5, 1, 130, 'http://www.clickjogos.com')
-card('img/hotel-4.jpg', 'Praia', "Florianópolis - SC" , 1, 5, 1, 980, 'https://pt.wikipedia.org/')
-
+card('img/hotel-1.jpg', 'Praia', "Porto Seguro"  , 1, 5, 1, 180, 'http://www.youtube.com')
+card('img/hotel-2.jpg', 'Praia', "Vitória"       , 1, 5, 1, 280, 'http://www.google.com')
+card('img/hotel-3.jpg', 'Praia', "São Luiz"      , 1, 5, 1, 130, 'http://www.clickjogos.com')
+card('img/hotel-4.jpg', 'Praia', "Florianópolis" , 1, 5, 1, 980, 'https://pt.wikipedia.org/')
+cards_places.children[0].remove()
 // slideshow
-setInterval(()=>{ capa.scrollTo(0,0*400) },2*1000)
-setInterval(()=>{ capa.scrollTo(0,1*400) },4*1000)
-setInterval(()=>{ capa.scrollTo(0,2*400) },6*1000)
-setInterval(()=>{ capa.scrollTo(0,3*400) },8*1000)
+n = 0
+quantity = capa.children.length
 
-d = new Date()
-ano.innerHTML = d.getFullYear()
+setInterval(()=>{ capa.scrollTo(capa.offsetWidth*(++n%quantity),0) }, 4*1000)
+
+// capa.scrollTo(capa.offsetWidth*0,0)
