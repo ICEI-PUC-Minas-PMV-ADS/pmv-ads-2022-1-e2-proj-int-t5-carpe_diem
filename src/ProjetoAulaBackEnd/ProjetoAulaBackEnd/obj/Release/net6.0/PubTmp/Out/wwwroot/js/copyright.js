69dd104a9dec84b61c99e68935b8aa5988ee89f0
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2022-06-12 11:23:08 -0300

    Fabi 12/06 11h

--- src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/obj/Release/net6.0/PubTmp/Out/wwwroot/js/copyright.js
+++
@@ -1,6 +0,0 @@
-let d = new Date()
-let year = d.getFullYear()
-
-for(i of document.querySelectorAll(".copyright")){
-    i.innerHTML = `© Copyright ${year} Carpe Diem - Todos os Direitos Reservados`
-}
