e43f429b9c007cedfd0e2cef039aa2555d64a7b1
Nayara <nayarakatlin@yahoo.com.br>
2022-06-12 22:18:09 -0300

    Inclusão model fale conosco

---
+++ src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/Models/FaleConosco.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAulaBackEnd.Models
+{
+    public class FaleConosco
+    {
+        [Required(ErrorMessage = ("Obrigatório informar o nome completo"))]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Deixe aqui sua mensagem.")]
+        [DisplayName("Mensagem")]
+        [StringLength(5000, MinimumLength = 20)]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+
+    }
+}
