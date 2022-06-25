10d63ffeef0cd0dcd7413d1d8e3bdd81ede62722
Cristiane F N Martins <cfnmartins@sga.pucminas.br>
2022-06-25 00:17:21 -0400

    Autorização Reserva

--- src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/Controllers/Reservas.cs
+++ src/ProjetoAulaBackEnd/ProjetoAulaBackEnd/Controllers/Reservas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using ProjetoAulaBackEnd.Models;
 
 namespace ProjetoAulaBackEnd.Controllers
 {
+    [Authorize]
     public class Reservas : Controller
     {
         private readonly Contexto _context;
